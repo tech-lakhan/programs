ba2cdf91aa3a1557615127de334c07039c081b94
“Lakhan” <tech.lakhan.it@gmail.com>
2024-02-17 17:13:32 +0530

    added required class to make test pass with basic method
    Signed-off-by: “Lakhan” <“tech.lakhan.it@gmail.com”>

---
+++ src/main/java/com/tech/poc/tdd/StringCalculator.java
@@ -0,0 +1,9 @@
+package com.tech.poc.tdd;
+
+public class StringCalculator {
+
+    public static int add(String numbers) {
+        int totalSum = 0;
+        return totalSum;
+    }
+}
--- src/test/java/com/tech/poc/tdd/StringCalculatorTest.java
+++ src/test/java/com/tech/poc/tdd/StringCalculatorTest.java
@@ -2,6 +2,8 @@ package com.tech.poc.tdd;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class StringCalculatorTest {
 
     @Test
