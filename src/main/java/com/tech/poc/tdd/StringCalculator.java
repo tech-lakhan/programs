5b0806f714fabea55b997380dd589ca50531781e
“Lakhan” <tech.lakhan.it@gmail.com>
2024-02-17 17:53:03 +0530

    refactored to handle test testAddCustomDelimiter
    Signed-off-by: “Lakhan” <“tech.lakhan.it@gmail.com”>

--- src/main/java/com/tech/poc/tdd/StringCalculator.java
+++ src/main/java/com/tech/poc/tdd/StringCalculator.java
@@ -8,8 +8,15 @@ public class StringCalculator {
         if (numbers.isEmpty()) {
             return 0;
         }
-        // Handle both commas and new lines as delimiters
-        String[] numArray = numbers.split("[,\n]");
+        String delimiter = ",";
+        if (numbers.startsWith("//")) {
+            String[] parts = numbers.split("\n", 2);
+            delimiter = parts[0].substring(2); // Extract custom delimiter
+            numbers = parts[1];
+        }
+        // Handle both the custom delimiter, commas, and new lines as delimiters
+        String[] numArray = numbers.split("[" + delimiter + "\n]");
+
         // Initialize sum
         int totalSum = 0;
         // Iterate through numbers
