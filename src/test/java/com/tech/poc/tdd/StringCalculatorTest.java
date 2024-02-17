585a0a57675c6323a6a0b60172b36c617a966f39
“Lakhan” <tech.lakhan.it@gmail.com>
2024-02-17 17:29:36 +0530

    added test testAddSingleNumber
    Signed-off-by: “Lakhan” <“tech.lakhan.it@gmail.com”>

--- src/test/java/com/tech/poc/tdd/StringCalculatorTest.java
+++ src/test/java/com/tech/poc/tdd/StringCalculatorTest.java
@@ -10,5 +10,9 @@ public class StringCalculatorTest {
     public void testAddEmptyString() {
         assertEquals(0, StringCalculator.add(""));
     }
+   @Test
+    public void testAddSingleNumber() {
+        assertEquals(1, StringCalculator.add("1"));
+    }
 
 }
