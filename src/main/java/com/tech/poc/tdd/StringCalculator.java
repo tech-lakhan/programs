bfbd753428a480b083869eb6fd317a0b775e2a37
“Lakhan” <tech.lakhan.it@gmail.com>
2024-02-17 17:31:11 +0530

    Refactored method add to handle a single number
    Signed-off-by: “Lakhan” <“tech.lakhan.it@gmail.com”>

--- src/main/java/com/tech/poc/tdd/StringCalculator.java
+++ src/main/java/com/tech/poc/tdd/StringCalculator.java
@@ -5,7 +5,8 @@ public class StringCalculator {
     public static int add(String numbers) {
         if (numbers.isEmpty()) {
             return 0;
+        }else {
+            return Integer.parseInt(numbers);
         }
-        return 0;
     }
 }
