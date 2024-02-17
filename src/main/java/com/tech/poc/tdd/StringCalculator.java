93360cf4bd0217202045ba38f4074a813c867cd9
“Lakhan” <tech.lakhan.it@gmail.com>
2024-02-17 17:39:35 +0530

    refactored add method to testAddTwoNumbers Handle two numbers
    Signed-off-by: “Lakhan” <“tech.lakhan.it@gmail.com”>

--- src/main/java/com/tech/poc/tdd/StringCalculator.java
+++ src/main/java/com/tech/poc/tdd/StringCalculator.java
@@ -1,12 +1,25 @@
 package com.tech.poc.tdd;
 
+import java.util.regex.Pattern;
+
 public class StringCalculator {
 
     public static int add(String numbers) {
         if (numbers.isEmpty()) {
             return 0;
-        }else {
-            return Integer.parseInt(numbers);
         }
+        String delimiter = ",";
+        // Split the numbers using the delimiter
+        String[] numArray = numbers.split(delimiter);
+        // Initialize sum
+        int totalSum = 0;
+        // Iterate through numbers
+        for (String num : numArray) {
+            if (!num.isEmpty()) {
+                int numInt = Integer.parseInt(num);
+                totalSum += numInt;
+            }
+        }
+        return totalSum;
     }
 }
