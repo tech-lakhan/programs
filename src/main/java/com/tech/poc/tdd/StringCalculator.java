39f12c9ed71d6325ce7019e40a1c64e510e860d3
“Lakhan” <tech.lakhan.it@gmail.com>
2024-02-17 17:28:08 +0530

    refactored add method logic for empty input
    Signed-off-by: “Lakhan” <“tech.lakhan.it@gmail.com”>

--- src/main/java/com/tech/poc/tdd/StringCalculator.java
+++ src/main/java/com/tech/poc/tdd/StringCalculator.java
@@ -3,7 +3,9 @@ package com.tech.poc.tdd;
 public class StringCalculator {
 
     public static int add(String numbers) {
-        int totalSum = 0;
-        return totalSum;
+        if (numbers.isEmpty()) {
+            return 0;
+        }
+        return 0;
     }
 }
